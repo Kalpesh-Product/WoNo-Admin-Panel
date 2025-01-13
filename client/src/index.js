833baaf5e0c4dd5ec078aff4867ca2d23d2d5360
sankalp-wono <sankalp.wono@gmail.com>
2025-01-13 11:56:10 +0530

    added auth in frontend

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { SidebarProvider } from './context/SideBarContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { SidebarProvider } from "./context/SideBarContext";
+import AuthContextProvider from "./context/AuthContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <AuthContextProvider>
       <SidebarProvider>
         <App />
       </SidebarProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
-
-
---
+++ client/src/pages/Loading.jsx
@@ -0,0 +1,8 @@
+export default function Loader() {
+    return (
+      <main className="w-full h-screen flex justify-center items-center">
+        <span className="w-12 h-12 border-4 border-black border-b-transparent rounded-full inline-block box-border animate-spin"></span>
+      </main>
+    );
+  }
+  
