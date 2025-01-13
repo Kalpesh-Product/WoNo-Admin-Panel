b146b757506cef5a5cc373830c6c48cfcee72867
allanwono <allan.wono@gmail.com>
2025-01-13 12:39:14 +0530

    minor changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,37 @@
 import { TextField, Avatar, InputAdornment } from "@mui/material";
-import { IoIosSearch, IoMdNotificationsOutline } from "react-icons/io";
+import {
+  IoIosArrowForward,
+  IoIosSearch,
+  IoMdNotificationsOutline,
+} from "react-icons/io";
 import { MdOutlineMailOutline } from "react-icons/md";
+import { useSidebar } from "../context/SideBarContext";
+import biznestLogo from "../assets/biznest/biznest_logo.jpg";
+import { GiHamburgerMenu } from "react-icons/gi";
+import Abrar from "../assets/abrar.jpeg";
 
 const Header = () => {
+  const { isSidebarOpen, setIsSidebarOpen } = useSidebar();
   return (
     <>
-      <div className="flex w-full justify-between gap-x-8 items-center p-2">
-        <div className="w-full flex items-center pl-6">
+      <div className="flex w-full justify-between gap-x-10 items-center p-2">
+        <div>
+          <div>
+            <div className={`w-40 flex items-center gap-10 h-full pl-4`}>
+              <img
+                className="w-[70%] h-full object-contain"
+                src={biznestLogo}
+                alt="logo"
+              />
+              <button
+                onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+                className="p-2 text-gray-500 text-xl">
+                {isSidebarOpen ? <GiHamburgerMenu /> : <IoIosArrowForward />}
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="w-full flex items-center pl-20">
           <TextField
             fullWidth
             size="small"
@@ -30,14 +55,16 @@ const Header = () => {
             <IoMdNotificationsOutline />
           </button>
           <button className="bg-[#1E3D73] p-2 text-white rounded-md">
-          <MdOutlineMailOutline />
+            <MdOutlineMailOutline />
           </button>
         </div>
         <div className="flex items-center gap-4 w-[40%]">
-          <Avatar>N</Avatar>
+          <Avatar>
+            <img src={Abrar} alt="" />
+          </Avatar>
           <div className="w-full">
-            <h1 className="text-xl font-semibold">Alex</h1>
-            <h2 className="text-md">Super Admin</h2>
+            <h1 className="text-xl font-semibold">Abrar Shaikh</h1>
+            <span className="text-content">Master Admin</span>
           </div>
         </div>
       </div>
