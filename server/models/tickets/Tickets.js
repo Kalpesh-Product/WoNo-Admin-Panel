dbab124c73f402d9eadc0a19f5833c438165d88e
sankalp-wono <sankalp.wono@gmail.com>
2025-01-16 10:44:07 +0530

    added ticket schemas

---
+++ server/models/tickets/Tickets.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const ticketsSchema = new mongoose.Schema(
+  {
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref:"TicketIssue"
+    },
+    raisedToDepartment: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Department",
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      default: "Pending",
+    },
+    assignees: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    resolvedDate: Date,
+  },
+  { timestamps: true }
+);
+
+const Ticket = mongoose.model("Ticket", ticketsSchema);
+module.exports = Ticket;
--- server/routes/ticketRoutes.js
+++ server/routes/ticketRoutes.js
@@ -1,38 +1,3 @@
-const ticketController = require("../controllers/ticketsControllers/ticketsControllers");
 const router = require("express").Router();
-
-// Create a new ticket
-router.post("/create-ticket", ticketController.createTicket);
-
-// View All Tickets
-router.get("/view-all-tickets", ticketController.fetchAllTickets);
-
-// View A single Tickets
-router.get("/view-a-single-ticket/:id", ticketController.fetchASingleTicket);
-
-// Delete Ticket
-router.delete("/delete-ticket/:id", ticketController.deleteTicket);
-
-// Edit Ticket
-router.put("/edit-ticket/:id", ticketController.updateTicket);
-
-// Accept Ticket
-router.put("/accept-ticket/:id", ticketController.acceptTicket);
-
-// Assign Ticket
-router.put("/assign-ticket/:id", ticketController.assignTicket);
-
-// Close Ticket
-router.put("/close-ticket/:id", ticketController.closeTicket);
-
-// Escalate Ticket
-router.put("/escalate-ticket/:id", ticketController.escalateTicket);
-
-// TICKET TEST USER ROUTES START
-
-// User Signup
-router.post("/signup-user", ticketController.signupUser);
-
-// TICKET TEST USER ROUTES END
 
 module.exports = router;
