2a4d7f1b4401ee66d2e3244d19bd89e8a2705384
anushriinfipre <anushri.infipre@gmail.com>
2025-01-20 17:14:04 +0530

    Changes

--- client/src/pages/Dashboard/HrDashboard/HrSettings/Shifts.jsx
+++ client/src/pages/Dashboard/HrDashboard/HrSettings/Shifts.jsx
@@ -1,11 +1,98 @@
-import React from 'react'
+import React from "react";
+import AgTable from "../../../../components/AgTable";
+import { Chip } from "@mui/material";
+
 
 const Shifts = () => {
+
+  const departmentsColumn = [
+      { field: "shiftlist", headerName: "Shift List",
+        cellRenderer:(params)=>{
+          return(
+            <div>
+              <span className="text-primary cursor-pointer hover:underline">
+                {params.value}
+              </span>
+            </div>
+          )
+        },flex:1},
+      {
+        field: "status",
+        headerName: "Status",
+        cellRenderer: (params) => {
+          const statusColorMap = {
+            Inactive: { backgroundColor: "#FFECC5", color: "#CC8400" }, // Light orange bg, dark orange font
+            Active: { backgroundColor: "#90EE90", color: "#006400" }, // Light green bg, dark green font
+          };
+  
+          const { backgroundColor, color } = statusColorMap[params.value] || {
+            backgroundColor: "gray",
+            color: "white",
+          };
+          return (
+            <>
+              <Chip
+                label={params.value}
+                style={{
+                  backgroundColor,
+                  color,
+                }}
+              />
+            </>
+          );
+        },flex:1
+      },
+      {
+        field: "actions",
+        headerName: "Actions",
+        cellRenderer: (params) => (
+          <>
+            <div className="p-2 mb-2 flex gap-2">
+             <span className="text-content text-primary hover:underline cursor-pointer">
+              Make Inactive
+             </span>
+            </div>
+          </>
+        ),
+      },
+    ];
+  
+  
+    const rows = [
+      {
+        id: 1,
+        shiftlist:"General Shift",
+        status: "Active",
+      },
+      {
+        id: 2,
+        shiftlist:"Night Shift",
+        status: "Active",
+      },
+      {
+        id: 3,
+        shiftlist:"Afternoon Shift",
+        status: "Inactive",
+      },
+      {
+        id: 4,
+        shiftlist:"Evening Shift",
+        status: "Active",
+      },
+      
+    ];
   return (
     <div>
-Shifts
+      <AgTable
+        search={true}
+        searchColumn={"Shifts"}
+        tableTitle={"Shift List"}
+        buttonTitle={"Add Shift List"}
+        data={rows}
+        columns={departmentsColumn}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Shifts
+export default Shifts;
--- client/src/pages/Dashboard/HrDashboard/HrSettings/WorkLocations.jsx
+++ client/src/pages/Dashboard/HrDashboard/HrSettings/WorkLocations.jsx
@@ -1,10 +1,98 @@
 import React from 'react'
+import AgTable from "../../../../components/AgTable";
+import { Chip } from "@mui/material";
 
 const WorkLocations = () => {
+
+  const departmentsColumn = [
+    { field: "worklocationname", headerName: "Work Location Name",
+      cellRenderer:(params)=>{
+        return(
+          <div>
+            <span className="text-primary cursor-pointer hover:underline">
+              {params.value}
+            </span>
+          </div>
+        )
+      },flex:1},
+    {
+      field: "status",
+      headerName: "Status",
+      cellRenderer: (params) => {
+        const statusColorMap = {
+          Inactive: { backgroundColor: "#FFECC5", color: "#CC8400" }, // Light orange bg, dark orange font
+          Active: { backgroundColor: "#90EE90", color: "#006400" }, // Light green bg, dark green font
+        };
+
+        const { backgroundColor, color } = statusColorMap[params.value] || {
+          backgroundColor: "gray",
+          color: "white",
+        };
+        return (
+          <>
+            <Chip
+              label={params.value}
+              style={{
+                backgroundColor,
+                color,
+              }}
+            />
+          </>
+        );
+      },flex:1
+    },
+    {
+      field: "actions",
+      headerName: "Actions",
+      cellRenderer: (params) => (
+        <>
+          <div className="p-2 mb-2 flex gap-2">
+           <span className="text-content text-primary hover:underline cursor-pointer">
+            Make Inactive
+           </span>
+          </div>
+        </>
+      ),
+    },
+  ];
+
+  const rows = [
+    {
+      id: 1,
+      worklocationname: "ST 701A",
+      status: "Active",
+    },
+    {
+      id: 2,
+      worklocationname: "ST 701B",
+      status: "Active",
+    },
+    {
+      id: 3,
+      worklocationname: "ST 601A",
+      status: "Inactive",
+    },
+    {
+      id: 4,
+      worklocationname: "ST 701B",
+      status: "Active",
+    },
+    
+  ];
+  
   return (
-    <div>
-WorkLocations
-    </div>
+    
+      <div>
+        <AgTable
+          search={true}
+          searchColumn={"Work Location"}
+          tableTitle={"Work Location List"}
+          buttonTitle={"Add Work Location"}
+          data={rows}
+          columns={departmentsColumn}
+        />
+      </div>
+    
   )
 }
 
