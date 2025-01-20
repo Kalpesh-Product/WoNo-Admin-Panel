29c8243548ed0ff8fc42e0bfdbb476b99ec3cfbd
sankalp-wono <sankalp.wono@gmail.com>
2025-01-20 18:40:55 +0530

    some changes

--- client/src/pages/Tickets/RaiseTicket.jsx
+++ client/src/pages/Tickets/RaiseTicket.jsx
@@ -198,7 +198,7 @@ const RaiseTicket = () => {
   const handleDepartmentSelect = async (e) => {
     try {
       const response = await axios.get(`/api/tickets/get-ticket-issue/${e}`);
-      setTicketIssues(response.data);
+      setTicketIssues(response.data.data);
       setSelectedDepartment(e);
     } catch (error) {
       toast.error(error?.message);
