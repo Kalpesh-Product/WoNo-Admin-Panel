f9daa9c594b782024709dc83861f7cba4c598ea2
AiwinrajWono <wono.uiuxdev.intern03@gmail.com>
2025-01-22 19:03:23 +0530

    Minor changes from the meeting

--- client/src/components/graphs/PieChartMui.jsx
+++ client/src/components/graphs/PieChartMui.jsx
@@ -1,37 +1,22 @@
 import React from "react";
-import { PieChart } from "@mui/x-charts/PieChart";
+import ReactApexChart from "react-apexcharts";
 
-const PieChartMui = ({ data, title }) => {
+const PieChartMui = ({ data, title, options, tooltipFormatter }) => {
+  // Extract series data for ApexCharts
+  const chartData = data.map((item) => parseFloat(item.value)); // Ensure values are numbers
+
   return (
     <div className="border-default border-borderGray rounded-md">
       <div className="gray-underline p-4">
-        <h1 className="text-xl">{title}</h1>
+        <h1 className="text-subtitle">{title}</h1>
       </div>
       <div className="w-full m-0 flex p-4">
-        <PieChart
-          series={[
-            {
-              arcLabel: (item) => `${item.label}`,
-
-              data: data.map((item) => ({
-                ...item,
-                value: item.value, // The value for the pie slice
-                name: item.name, // Optional, for labels or legends
-                backgroundColor: item.color, // Use the color from the data
-              })),
-            },
-          ]}
+        <ReactApexChart
+          options={options} // Use options passed directly from parent
+          series={chartData} // Data values for the pie slices
+          type="pie"
           width={500}
           height={300}
-          slotProps={{
-            legend: {
-              direction: "column",
-              position: {
-                horizontal: "right",
-                vertical: "top",
-              },
-            },
-          }}
         />
       </div>
     </div>
