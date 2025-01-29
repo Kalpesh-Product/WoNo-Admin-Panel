b7753407cd0ad23ad5c7d9bcef11ebdd191984e5
AiwinrajWono <wono.uiuxdev.intern03@gmail.com>
2025-01-29 12:19:59 +0530

    meetings menu created

---
+++ client/src/pages/Meetings/MeetingDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MeetingDashboard = () => {
+  return (
+    <div className='font-pregular'>
+      Dash here
+    </div>
+  )
+}
+
+export default MeetingDashboard
