843f19c94ce83a4cca8e31831ba9427c566f8251
muskan-wono <muskan.wono@gmail.com>
2025-02-12 15:58:24 +0530

    Integrated reviews with backend

--- client/src/pages/Meetings/Reviews.jsx
+++ client/src/pages/Meetings/Reviews.jsx
@@ -8,10 +8,12 @@ import { PiArrowBendLeftDownBold } from "react-icons/pi";
 import MuiAside from "../../components/MuiAside";
 import PrimaryButton from "../../components/PrimaryButton";
 import TextField from "@mui/material/TextField";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+ 
 
 const Reviews = () => {
+  const axios = useAxiosPrivate()
   const [openSidebar, setOpenSidebar] = useState(false);
   const [reviewData, setReviewData] = useState({});
 
@@ -84,6 +86,23 @@ const Reviews = () => {
           }
         };
 
+        
+        //         const {mutate}=useMutation({
+        //           mutationFn:async(data)=>{
+        //             const response= await axios.post("/api/meetings/add-reply");
+        //           },
+        //           onSuccess:()=>{
+        //             toast.success("review added successfully");
+        //           },onError:()=>{
+        //             toast.error("could not add review")
+        //           }
+        //         })
+        // const handleSubmitReview = (data) => {
+        //   mutate(data)
+
+        //   setOpenSidebar(false)
+        // }
+
         return (
           <>
             <Chip
@@ -151,12 +170,16 @@ const Reviews = () => {
     },
   ];
 
+  const averageRatings = reviews.reduce((acc,curr)=> acc + curr.rate,0) / reviews.length
+
+  // const averageRatings = rating
+
   return (
     <>
       <div>
         <WidgetSection layout={3}>
-          <DataCard data="10.0k" title="Total" description="Reviews Count" />
-          <DataCard data="4.5⭐" title="Average" description=" Ratings" />
+          <DataCard data={reviews.length} title="Total" description="Reviews Count" />
+          <DataCard data={`${averageRatings} ⭐`} title="Average" description=" Ratings" />
           <DataCard data="10.0k" title="Total" description="Reviews Count" />
         </WidgetSection>
 
@@ -164,7 +187,19 @@ const Reviews = () => {
           <AgTable
             search={true}
             searchColumn={"Policies"}
-            data={rows}
+            data={[
+              ...reviews.map((review,index) => (
+                {
+                  id: index + 1,
+                  srno: index + 1,
+      nameofreview: review.reviewerName,
+      date: new Intl.DateTimeFormat("en-GB",{day:"numeric",month:"long",year:"numeric"}).format(new Date(review.meeting.startDate)),
+      rate: review.rate,
+      Reviews: review.review,
+      action: review?.reply ? "Replied" : "Reply Review",
+                }
+              ))
+            ]}
             columns={departmentsColumn}
           />
         </div>
@@ -175,13 +210,13 @@ const Reviews = () => {
         >
           <div className="p-2">
             <h1 className="font-pmedium text-subtitle">
-              {reviewData.nameofreview}
+              {reviewData.reviewerName}
             </h1>
             <div>
               ⭐ {reviewData.rate} <small> out of 5</small>
             </div>
             <div className="mt-10">
-              <p>{reviewData.Reviews}</p>
+              <p>{reviewData.review}</p>
             </div>
             <div className="mt-5">
               <TextField
