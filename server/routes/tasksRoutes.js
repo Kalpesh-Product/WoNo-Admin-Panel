3b636b65a12b43201b384a4b55cf4a4dfff68524
muskan-wono <muskan.wono@gmail.com>
2025-02-22 10:17:12 +0530

    Fixed typo in task route

--- server/routes/tasksRoutes.js
+++ server/routes/tasksRoutes.js
@@ -16,13 +16,13 @@ const {
 } = require("../controllers/tasksControllers/tasksControllers");
 
 router.post("/create-project", createProject);
-router.post("/create-tasks", createTasks);
 router.get("/get-projects", getProjects);
-router.put("/create-project/:id", updateProject);
-router.put("/update-task/:id", updateTask);
+router.put("/update-project/:id", updateProject);
+router.delete("/delete-project/:id", deleteProject);
+router.post("/create-tasks", createTasks);
 router.get("/get-tasks", getTasks);
 router.get("/get-today-tasks", getTodayTasks);
-router.delete("/delete-project/:id", deleteProject);
+router.put("/update-task/:id", updateTask);
 router.delete("/delete-task/:id", deleteTask);
 
 module.exports = router;
