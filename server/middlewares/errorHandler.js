902313d83e1c57b13511a8fe09f099dd957f2c57
muskan-wono <muskan.wono@gmail.com>
2025-03-06 10:51:57 +0530

    Cleaned up customErrorLogs

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -6,52 +6,8 @@
 
 const { createLog } = require("../utils/moduleLogs");
 
-// const errorHandler = async (err, req, res) => {
-//   try {
-//     const { user, ip, company } = req;
-
-//     const statusCode = err.statusCode || 500;
-//     const message = err.message || "Internal Server Error";
-//     const path = err.path || req.originalUrl || "";
-//     const action = err.action || req.originalUrl || "Unknown API Error";
-//     const sourceKey = err.sourceKey || "";
-//     const sourceId = null;
-//     const remarks = err.message;
-
-//     console.log("statusCode", statusCode);
-//     if (req.method !== "GET") {
-//       try {
-//         await createLog({
-//           path,
-//           action,
-//           remarks,
-//           user,
-//           ip,
-//           company,
-//           sourceKey,
-//           sourceId,
-//         });
-//       } catch (logError) {
-//         return res
-//           .status(500)
-//           .json({ message: "Logging failed", error: logError });
-//       }
-//     }
-
-//     return res.status(statusCode).json({ message, path });
-//   } catch (criticalError) {
-//     return res.status(500).json({
-//       message: "Critical error in error handler",
-//       error: criticalError,
-//     });
-//   }
-// };
-
 const errorHandler = async (err, req, res, next) => {
   try {
-    console.log("🔴 ErrorHandler called"); // Confirm it's triggered
-    console.error("🔴 Error:", err); // Log full error
-
     const { user, ip, company } = req;
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error";
@@ -74,7 +30,6 @@ const errorHandler = async (err, req, res, next) => {
           sourceId,
         });
       } catch (logError) {
-        console.error("🔴 Logging Error in errorHandler:", logError);
         return res
           .status(500)
           .json({ message: "Logging failed", error: logError });
@@ -83,7 +38,6 @@ const errorHandler = async (err, req, res, next) => {
 
     return res.status(statusCode).json({ message });
   } catch (criticalError) {
-    console.error("🔴 CRITICAL ERROR in errorHandler:", criticalError);
     return res.status(500).json({
       message: "Critical error in error handler",
       error: criticalError,
--- server/utils/customErrorlogs.js
+++ server/utils/customErrorlogs.js
@@ -6,11 +6,6 @@ class CustomError extends Error {
     sourceKey = "",
     statusCode = 400
   ) {
-    console.log("message", message);
-    console.log("path", path);
-    console.log("action", action);
-    console.log("sourceKey", sourceKey);
-
     super(message);
     this.statusCode = statusCode;
     this.path = path;
