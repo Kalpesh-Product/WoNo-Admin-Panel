498e606e16343947dabb0d1be97b6d5b22ee900e
sankalp-wono <sankalp.wono@gmail.com>
2025-03-07 14:23:15 +0530

    added update profile controller

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -17,7 +17,7 @@ router.patch("/employee-leaves", addEmployeeLeaves);
 router.get("/fetch-users", fetchUser);
 router.get("/assignees", getAssignees);
 router.get("/fetch-single-user/:empid", fetchSingleUser);
-router.patch("/update-single-user",upload.single("profile-pic"), updateProfile);
+router.patch("/update-single-user",upload.single("profilePic"), updateProfile);
 router.post("/bulk-insert-users", upload.single("users"), bulkInsertUsers);
 
 module.exports = router;
