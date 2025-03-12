c75c08ec44f264e399539e29d8897a38fb7c4253
sankalp-wono <sankalp.wono@gmail.com>
2025-03-12 12:42:52 +0530

    some changes

--- server/models/budget/Budget.js
+++ server/models/budget/Budget.js
@@ -1,38 +1,38 @@
 const mongoose = require("mongoose");
 
-const budgetSchema = new mongoose.Schema(
-  {
-    expenseName: {
-      type: String,
-      required: true,
-    },
-    department: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Department",
-    },
-    company: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Company",
-    },
-    expenseType: {
-      type: String,
-      required: true,
-    },
-    amount: {
-      type: Number,
-      required: true,
-    },
-    dueDate: {
-      type: Date,
-      required: true,
-    },
-    status: {
-      type: String,
-      default: "Pending",
-    },
+const budgetSchema = new mongoose.Schema({
+  department: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Department",
   },
-  { timestamps: true }
-);
+  company: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Company",
+  },
+  ammount: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["Pending", "Approved", "Rejected"],
+    default: "Pending",
+  },
+  utilized: {
+    type: Number,
+    default: 0,
+  },
+  requests: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "RequestBudget",
+    },
+  ],
+  forMonth: {
+    type: Date,
+    required: true,
+  },
+});
 
 const Budget = mongoose.model("Budget", budgetSchema);
 module.exports = Budget;
