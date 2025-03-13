3a1cea0f512e7e2b21ad5bd22148b3744c02ad67
sankalp-wono <sankalp.wono@gmail.com>
2025-03-13 18:16:14 +0530

    some changes

---
+++ server/models/sales/Leads.js
@@ -0,0 +1,100 @@
+const mongoose = require("mongoose");
+
+const LeadSchema = new mongoose.Schema(
+  {
+    dateOfContact: {
+      type: Date,
+      required: true,
+    },
+    companyName: {
+      type: String,
+      required: true,
+    },
+    salesCategory: {
+      type: String,
+      required: true,
+    },
+    leadStatus: {
+      type: String,
+      required: true,
+    },
+    proposedLocation: {
+      type: String,
+      required: true,
+    },
+    source: {
+      type: String,
+      required: true,
+    },
+    sector: {
+      type: String,
+      required: true,
+    },
+    hoLocation: {
+      type: String,
+      required: true,
+    },
+    officeInGoa: {
+      type: Boolean,
+      required: true,
+    },
+    pocName: {
+      type: String,
+      required: true,
+    },
+    designation: {
+      type: String,
+      required: true,
+    },
+    contactNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    leadSource: {
+      type: String,
+      required: true,
+    },
+    period: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    openDesks: {
+      type: Number,
+      required: true,
+    },
+    cabinDesks: {
+      type: Number,
+      required: true,
+    },
+    totalDesks: {
+      type: Number,
+      required: true,
+    },
+    clientBudget: {
+      type: Number,
+      required: true,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    remarksComments: {
+      type: String,
+    },
+    lastFollowUpDate: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Lead", LeadSchema);
