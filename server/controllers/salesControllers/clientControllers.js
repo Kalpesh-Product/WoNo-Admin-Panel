fa25d51c1a5f95d28974249fe8bc1ad26554bc25
muskan-wono <muskan.wono@gmail.com>
2025-03-15 16:56:43 +0530

    Updated onboard client route

--- server/controllers/salesControllers/clientControllers.js
+++ server/controllers/salesControllers/clientControllers.js
@@ -7,179 +7,121 @@ const { PDFDocument } = require("pdf-lib");
 
 //Onboard a new client
 
-const upload = multer({ storage: multer.memoryStorage() }).fields([
-  { name: "companyCertificateOfIncorporation", maxCount: 1 },
-  { name: "companyGSTCertificate", maxCount: 1 },
-  //   { name: "companyListofDirectors", maxCount: 1 },
-  //   { name: "detailsOfTheSigningAuthority", maxCount: 1 },
-]);
-
 const createClient = async (req, res) => {
-  upload(req, res, async (err) => {
-    if (err) return res.status(400).json({ message: err.message });
+  try {
+    const { company } = req;
+    const {
+      clientName,
+      sector,
+      hoCity,
+      hoState,
+      unit,
+      cabinDesks,
+      openDesks,
+      totalDesks,
+      ratePerDesk,
+      annualIncrement,
+      perDeskMeetingCredits,
+      totalMeetingCredits,
+      startDate,
+      endDate,
+      lockinPeriod,
+      rentDate,
+      nextIncrement,
+      localPocName,
+      localPocEmail,
+      localPocPhone,
+      hOPocName,
+      hOPocEmail,
+      hOPocPhone,
+    } = req.body;
 
-    try {
-      const {
-        company,
-        clientName,
-        sector,
-        hoCity,
-        hoState,
-        unit,
-        cabinDesks,
-        openDesks,
-        totalDesks,
-        ratePerDesk,
-        annualIncrement,
-        perDeskMeetingCredits,
-        totalMeetingCredits,
-        startDate,
-        endDate,
-        lockinPeriod,
-        rentDate,
-        nextIncrement,
-        pocName,
-        companyRegisteredAddress,
-        companyPANCardNumber,
-      } = req.body;
+    //Check if client already exists
 
-      //Check if client already exists
+    const clientExists = await Client.findOne({ clientName });
 
-      const clientExists = await Client.findOne({ clientName });
-
-      if (clientExists) {
-        return res.status(400).json({ message: "Client already exists" });
-      }
-      // Basic validation
-      if (
-        !clientName ||
-        !sector ||
-        !hoCity ||
-        !hoState ||
-        !totalDesks ||
-        !ratePerDesk ||
-        !annualIncrement ||
-        !startDate ||
-        !endDate ||
-        !lockinPeriod ||
-        !rentDate ||
-        !nextIncrement ||
-        !pocName ||
-        !companyRegisteredAddress ||
-        !companyPANCardNumber
-      ) {
-        return res
-          .status(400)
-          .json({ message: "All required fields must be provided" });
-      }
-
-      if (totalDesks < 1 || ratePerDesk <= 0 || annualIncrement < 0) {
-        return res.status(400).json({ message: "Invalid numerical values" });
-      }
-
-      if (new Date(startDate) >= new Date(endDate)) {
-        return res
-          .status(400)
-          .json({ message: "Start date must be before end date" });
-      }
-
-      const foundCompany = await Company.findById(company);
-
-      if (!foundCompany) {
-        return res.status(400).json({ message: "Company not found" });
-      }
-
-      // Upload documents if they exist
-      const uploadedDocuments = {};
-      const fileFields = [
-        "companyCertificateOfIncorporation",
-        "companyGSTCertificate",
-        "companyListofDirectors",
-        "detailsOfTheSigningAuthority",
-      ];
-
-      for (const field of fileFields) {
-        if (req.files[field]) {
-          const file = req.files[field][0]; // Extract single file from array
-
-          //validate file type
-          try {
-            if (!file.mimetype.includes("pdf")) {
-              throw new Error("Uploaded file is not a valid PDF");
-            }
-
-            const pdfDoc = await PDFDocument.load(file.buffer);
-            pdfDoc.setTitle(
-              file.originalname ? file.originalname.split(".")[0] : "Untitled"
-            );
-            const processedBuffer = await pdfDoc.save();
-
-            const response = await handleDocumentUpload(
-              processedBuffer,
-              `${foundCompany.companyName}/clients/${clientName}/documents/${field}`,
-              file.originalname
-            );
-
-            if (!response.public_id) {
-              return res
-                .status(500)
-                .json({ message: `Failed to upload ${field}` });
-            }
-
-            uploadedDocuments[field] = {
-              name: file.originalname,
-              documentLink: response.secure_url,
-              documentId: response.public_id,
-            };
-          } catch (error) {
-            console.error("Error processing PDF:", error.message);
-            return res.status(400).json({ message: "Invalid PDF file" });
-          }
-        }
-      }
-
-      // Create client object
-      const client = new Client({
-        company,
-        clientName,
-        sector,
-        hoCity,
-        hoState,
-        unit,
-        cabinDesks,
-        openDesks,
-        totalDesks,
-        ratePerDesk,
-        annualIncrement,
-        perDeskMeetingCredits,
-        totalMeetingCredits,
-        startDate,
-        endDate,
-        lockinPeriod,
-        rentDate,
-        nextIncrement,
-        pocName,
-        companyRegisteredAddress,
-        companyPANCardNumber,
-        ...uploadedDocuments,
-      });
-
-      await client.save();
-      res
-        .status(201)
-        .json({ message: "Client onboarded successfully", client });
-    } catch (error) {
-      res.status(400).json({ message: error.message });
+    if (clientExists) {
+      return res.status(400).json({ message: "Client already exists" });
     }
-  });
+    // Basic validation
+    if (
+      !clientName ||
+      !sector ||
+      !hoCity ||
+      !hoState ||
+      !totalDesks ||
+      !ratePerDesk ||
+      !annualIncrement ||
+      !startDate ||
+      !endDate ||
+      !lockinPeriod ||
+      !rentDate ||
+      !nextIncrement ||
+      !localPocName ||
+      !localPocEmail ||
+      !localPocPhone ||
+      !hOPocName ||
+      !hOPocEmail ||
+      !hOPocPhone
+    ) {
+      return res
+        .status(400)
+        .json({ message: "All required fields must be provided" });
+    }
+
+    if (totalDesks < 1 || ratePerDesk <= 0 || annualIncrement < 0) {
+      return res.status(400).json({ message: "Invalid numerical values" });
+    }
+
+    if (new Date(startDate) >= new Date(endDate)) {
+      return res
+        .status(400)
+        .json({ message: "Start date must be before end date" });
+    }
+
+    // Create client object
+    const client = new Client({
+      company,
+      clientName,
+      sector,
+      hoCity,
+      hoState,
+      unit,
+      cabinDesks,
+      openDesks,
+      totalDesks,
+      ratePerDesk,
+      annualIncrement,
+      perDeskMeetingCredits,
+      totalMeetingCredits,
+      startDate,
+      endDate,
+      lockinPeriod,
+      rentDate,
+      nextIncrement,
+      hOPoc: {
+        name: hOPocName,
+        email: hOPocEmail,
+        phone: hOPocPhone,
+      },
+      localPoc: {
+        name: localPocName,
+        email: localPocEmail,
+        phone: localPocPhone,
+      },
+    });
+
+    await client.save();
+    res.status(201).json({ message: "Client onboarded successfully" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 // Get all clients with validation
 const getClients = async (req, res, next) => {
   try {
-    const clients = await Client.find({ isActive: true }).populate(
-      "company unit"
-    );
+    const clients = await Client.find().populate("unit");
     if (!clients.length) {
       return res.status(404).json({ message: "No clients found" });
     }
--- server/routes/salesRoutes.js
+++ server/routes/salesRoutes.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const {
   createClient,
+  getClients,
 } = require("../controllers/salesControllers/clientControllers");
 
 router.post("/onboard-client", createClient);
+router.get("/clients", getClients);
 
 module.exports = router;
